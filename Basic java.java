3b5ffa7dc1bba90c4716a8de138ae9dda43853d2
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-03-22 10:23:36 +0530

    main

---
+++ Basic java.java
@@ -0,0 +1,124 @@
+//Java
+
+//History of Java:
+
+//James Gosling(Father of Java)
+//Sun Microsystems(Company)
+//1995(Year)
+//Oak(Original Name)
+//Green Project(Project Name)
+
+//Define/What is Java?
+
+
+//Java is a most popular programming language.
+//Java is a high-level, class-based, 
+//Java is an object-oriented programming language that is designed to have as few implementation dependencies as possible.
+//Java is a general-purpose programming language that is class-based, object-oriented, concurrent, and designed to have as few implementation dependencies as possible.
+//Java has multithreading, garbage collection, and exception handling.
+//Java is a platform-independent language.
+//Java is most used language in the world.
+//Java is a WORA(Write Once Run Anywhere) language.(Because of JVM and JRE)
+//Java is more secure language.(Because of JVM)
+
+//Features of Java:
+
+//Java is a simple language.
+//Java is a robust language.
+//Java is a secure language.
+//Java is a portable language.
+//Java is a dynamic language.
+//Java is a distributed language.
+//Java is a high-performance language.
+//Java is a multithreaded language.
+//Java is a garbage-collected language.
+//Java is an exception-handling language.
+//Java is a platform-independent language.
+
+//Java Environment:
+
+//JDK(Java Development Kit)
+//JRE(Java Runtime Environment)
+//JVM(Java Virtual Machine)
+
+//JDK = JRE + Development Tools
+//JRE = JVM + Class Libraries
+//JVM = Java Virtual Machine
+
+//How to run java program?
+//1. Write the code
+//2. Compile the code
+//3. Run the code
+
+//Runing process:
+
+//Source Code -> Compiler ->Byte Code -> JVM -> Machine code
+
+//Run in terminal:
+
+//1. Open terminal
+//2. Navigate to the directory where the file is saved
+//3. Compile the code
+//4. Run the code
+
+//Example:
+
+//javac filename.java
+//java filename
+
+//Syntax of Java:
+
+/*
+class classname{
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
+*/
+
+//Output:
+
+//Hello Java
+
+//Explanation of Syntax:
+/*
+class(Blueprint)
+public(Access Modifier)
+static(Method)
+void(Return Type)
+main(Method)
+String[] args(Array)
+System.out.println(Print Statement)
+*/
+
+//Example:
+
+class BasicJava{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+//Run in terminal:
+
+/*javac BasicJava.java
+java BasicJava
+*/
+
+//Runing process:
+
+/*Write code → Save (.java)
+        ↓
+Compile → javac BasicJava.java
+        ↓
+Bytecode → BasicJava.class
+        ↓
+Run → java BasicJava
+        ↓
+Output → Hello World
+
+*/
+
+//Output:
+
+//Hello World
