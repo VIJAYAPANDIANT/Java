2c9690f19bfb003771217f40aeb22ca4e94e7b3a
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-04-05 07:20:59 +0530

    main

---
+++ comment.java
@@ -0,0 +1,49 @@
+//Comments in Java:
+
+//Define:
+//Comments are the statements that are ignored by the compiler.
+//Comments are used to explain the code.
+//Comments are used to make the code more readable.
+//comment line used to human understanding the code
+
+//Types of Comments:
+
+//1. Single-line comment:
+
+//This is a single-line comment
+
+//2. Multi-line comment:
+
+/*
+This is a multi-line comment
+*/
+
+//3. Documentation comment:
+
+/**
+This is a documentation comment
+*/
+
+//Example:
+
+class Comment{
+    public static void main(String[] args){
+        //Single-line comment
+        System.out.println("Hello World");
+        /*
+        Multi-line comment
+        */
+        System.out.println("Hello Java");
+        /**
+        Documentation comment
+        */
+        System.out.println("Hello Comment");
+    }
+}
+
+//Output:
+
+//Hello World
+//Hello Java
+//Hello Comment
+
