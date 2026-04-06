1b54c0ccbd24f376a04e70ff970fbf547331c0b7
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-04-06 17:54:00 +0530

    main

---
+++ Datatype.java
@@ -0,0 +1,56 @@
+//Datatype in Java:
+
+//Datatype is a type of data that can be stored in a variable.
+//Datatype is a type of data that can be processed by a program.
+//Datatype is a type of data that can be manipulated by a program.
+
+//Types of Datatype:
+
+//1. Primitive Datatype
+//2. Non-Primitive Datatype
+
+//Primitive Datatype:
+
+//int
+//double
+//float
+//char
+//boolean
+//long
+//short
+//byte
+
+//Example:
+
+class Datatype{
+    public static void main(String[] args){
+        //Primitive Datatype
+        int a = 10;
+        double b = 20.5;
+        float c = 30.5f;
+        char d = 'A';
+        boolean e = true;
+        long f = 40;
+        short g = 50;
+        byte h = 60;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
+
+//Output:
+
+//10
+//20.5
+//30.5
+//A
+//true
+//40
+//50
+//60
