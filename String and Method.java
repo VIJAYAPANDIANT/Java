ead6922d62494a2794a0b991759a7564b81f0544
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-04-17 06:41:45 +0530

    main

---
+++ String and Method.java
@@ -0,0 +1,18 @@
+/*
+
+String in Java
+
+Definition:
+
+A String in Java is a sequence of characters used to store and manipulate text.
+It is an object of the String class.
+
+Syntax:
+
+String variableName = "value";
+
+Example:
+
+String name = "Vijay";
+
+*/
